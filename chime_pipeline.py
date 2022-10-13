c64abe89b8b3917b422fd00d195b35a7a90127d7
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-10-13 12:12:19 -0500

    Refactor and parametrize

--- chime_pipeline.py
+++ chime_pipeline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from ast import parse
 import glob
 import os
 import getopt
@@ -9,29 +10,53 @@ from pplib import *
 import pptoas as ppt
 import ppalign
 import subprocess
+import argparse
+import json
 
 log.setLevel("INFO")
+
+class PulsarConfig:
+    def __init__(self, name:str, dm:float, model_portrit:str, nchan:int, nsub:int, zap_chans:str):
+        self.name = name
+        self.dm = dm
+        self.model_portrit = model_portrit
+        self.nchan = nchan
+        self.nsub = nsub
+        self.zap_chans = zap_chans
+        self.datafile_glob_prefix = f"CHIME_{self.name}_beam_?_?????_?????"
 
 class Session:
     """Class for storing command line arguments and other global state."""
     
-    def __init__(self, argv):
-        cmdargs = sys.argv[1:]
-        opts, args = getopt.gnu_getopt(cmdargs, "", [])
+    def __init__(self):
+        
+        parser = argparse.ArgumentParser(description="Generate TOAs from fold mode CHIME data.")
+        parser.add_argument("-i", "--input_dir", required=True, help="Directory where input archives are stored.")
+        parser.add_argument("-o", "--ouput_dir", required=True, help="Directory where output files will be stored.")
+        parser.add_argument("-c", "--config", required=True, help="Configuration file (JSON format).")
+        parser.add_argument("-t", "--test", required=False, dest="test_mode", action="store_true", help="Run in test mode (don't execute commands, display only).")
+        args = parser.parse_args()
+        
+        #cmdargs = sys.argv[1:]
+        #opts, args = getopt.gnu_getopt(cmdargs, "", [])
 
         #Run this script as follows: python chime_pipeline.py "path to .ar files" "path were data products are to be stored" "psr jname" "post scrunch zapping file"
-        self.input_dir = os.path.realpath(args[0])
-        self.output_dir = os.path.realpath(args[1])
-        self.psrname = args[2]
-        self.dm = float(args[3])
-        self.model_portrait = args[4]
-        self.test_mode = False
-        #meta_file = args[5]
-        #pzap_file = args[4]
-    
-        self.datafile_glob_prefix = f"CHIME_{self.psrname}_beam_?_?????_?????"
+        self.input_dir = os.path.realpath(args.input_dir)
+        self.output_dir = os.path.realpath(args.ouput_dir)
+        self.config_file = os.path.realpath(args.config)
+        self.test_mode = args.test_mode
 
-def run_cmd(cmd, test_mode):
+        self.process_config()
+    
+    def process_config(self):
+        with open(self.config_file, 'r') as cf:
+            config_data = json.load(cf)
+            try:
+                self.pulsars = [PulsarConfig(**psr_data) for psr_data in config_data]
+            except:
+                raise AttributeError("The JSON config is malformed or missing attributes.")
+
+def run_cmd(cmd:str, test_mode:bool):
     """Run a shell command using Popen"""
     try:
         log.info(cmd)
@@ -41,32 +66,46 @@ def run_cmd(cmd, test_mode):
     except:
         raise OSError(f"Error while executing command.\ncmd :: {cmd}")
 
+def create_metafile(session:Session, pulsar:PulsarConfig):
+    """Make a metafile of the fully zapped and scrunched files"""
+    pzap_files = glob.glob(f"{session.output_dir}/{pulsar.datafile_glob_prefix}.pzap")
+    
+    meta_file = f"{session.output_dir}/{pulsar.name}.meta"
+
+    with open(meta_file, "w") as metafile:
+        for pzap_file in pzap_files:
+            metafile.write(f"{pzap_file}\n")
+
+    return meta_file
+
 if __name__ == "__main__":
 
     session = Session(sys.argv)
 
-    #Run psrsh in a loop to avoid memory issues
-    input_ar_files = glob.glob(f"{session.input_dir}/{session.datafile_glob_prefix}.ar")
-    for ar_file in input_ar_files:
-        prefix = os.path.splitext(os.path.basename(ar_file))[0]
+    for pulsar in session.pulsars:
+        log.info(f"Processing {pulsar.name}...")
+    
+        #Run psrsh in a loop to avoid memory issues
+        input_ar_files = glob.glob(f"{session.input_dir}/{pulsar.datafile_glob_prefix}.ar")
+        for ar_file in input_ar_files:
+            prefix = os.path.splitext(os.path.basename(ar_file))[0]
+            
+            zap_cmd = f"psrsh chime_convert_and_tfzap.psh -e zap -O {session.output_dir} {session.input_dir}/{prefix}.ar"
+            run_cmd(zap_cmd, session.test_mode)
+
+            #Command to scrunch to 64 frequency channels
+            scr_cmd = f"pam -e ftscr -u {session.output_dir} --setnchn {pulsar.nchan} --setnsub {pulsar.nsub} -d {pulsar.dm} {session.output_dir}/{prefix}.zap"
+            run_cmd(scr_cmd, session.test_mode)
+
+            #Post scrunching zapping. Will need to be unique per source
+            pzap_cmd = f'paz -z "{pulsar.zap_chans}" -e pzap -O {session.output_dir} {session.output_dir}/{prefix}.ftscr'
+            run_cmd(pzap_cmd, session.test_mode)
+
+
+        #Make a metafile of the fully zapped and scrunched files
+        meta_file = create_metafile(session, pulsar)
         
-        zap_cmd = f"psrsh chime_convert_and_tfzap.psh -e zap -O {session.output_dir} {session.input_dir}/{prefix}.ar"
-        run_cmd(zap_cmd, session.test_mode)
-
-        #Command to scrunch to 64 frequency channels
-        scr_cmd = f"pam -e ftscr -u {session.output_dir} --setnchn 64 --setnsub 1 -d {session.dm} {session.output_dir}/{prefix}.zap"
-        run_cmd(scr_cmd, session.test_mode)
-
-        #Post scrunching zapping. Will need to be unique per source
-        pzap_cmd = f'paz -z "17 36 15 7 2 0 1 5 47 40 34" -e pzap -O {session.output_dir} {session.output_dir}/{prefix}.ftscr'
-        run_cmd(pzap_cmd, session.test_mode)
-
-
-    #Make a metafile of the fully zapped and scrunched files
-    metafile_cmd = f'ls {session.output_dir}/{session.datafile_glob_prefix}.pzap > {session.output_dir}/{session.psrname}.meta'
-    os.system(metafile_cmd)
-    meta_file = f"{session.output_dir}/{session.psrname}.meta"
-    #print(meta_file)
-    gt = ppt.GetTOAs(meta_file, session.model_portrait)
-    gt.get_TOAs(DM0=session.dm)
+        #print(meta_file)
+        #gt = ppt.GetTOAs(meta_file, session.model_portrait)
+        #gt.get_TOAs(DM0=session.dm)
 

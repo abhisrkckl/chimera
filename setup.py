00f2fd7dc09bccbe8687b4e98eaa9c9d2d11dd84
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-12-06 13:58:40 -0600

    add versioneer

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
+import versioneer
 
 setup(
+    version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass(),
     scripts=["scripts/chimerawb", "scripts/chime_convert_and_tfzap.psh"],
 )

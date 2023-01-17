0d4f6a5725329772f451ddd3f3a08db0b1582a92
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-01-17 16:14:51 -0600

    black

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 import versioneer
 
 setup(
-    version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     scripts=["scripts/chimerawb", "scripts/chime_convert_and_tfzap.psh"],
 )

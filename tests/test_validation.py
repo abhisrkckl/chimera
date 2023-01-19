4849bc8629a2c298674643ab9b696e8a213dd341
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-01-19 16:32:24 -0600

    --

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,11 +1,10 @@
 import os
 
-# from chimerawb.validation import test_read_dir as validate_read_dir
+from chimerawb.validation import test_read_dir as validate_read_dir
 
 def test_validate_read_dir(tmpdir):
-    pass 
-    # testdir = tmpdir / "_0123_test_dir_"
-    # os.mkdir(testdir)
+    testdir = tmpdir / "_0123_test_dir_"
+    os.mkdir(testdir)
 
-    # assert validate_read_dir(testdir) == testdir
+    assert validate_read_dir(testdir) == testdir
     
